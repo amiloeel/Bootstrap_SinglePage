b66eeb7989b7fe423ac4dc4e3271fff81001af8a
Leandro Lima <90478919+amiloeel@users.noreply.github.com>
2022-01-11 10:10:30 -0300

    adicionando scroll animado

--- js/scripts.js
+++ js/scripts.js
@@ -113,5 +113,40 @@ $(document).ready(function(){
             });
         }
     }
+
+    //scroll 
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let productsSection = $('#products-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu'){
+            scrollTo = aboutSection;
+        }else if (btnId == 'services-menu'){
+            scrollTo = servicesSection;
+        }else if (btnId == 'team-menu'){
+            scrollTo = teamSection;
+        }else if (btnId == 'portfolio-menu'){
+            scrollTo = productsSection;
+        }else if (btnId == 'contact-menu'){
+            scrollTo = contactSection;
+        }else{
+            scrollTo = bannerSection;
+        }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+
+    });
     
 });
